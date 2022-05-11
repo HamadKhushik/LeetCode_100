968528828add05fb4b1d91981448eef9dba0e6e7
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-05-12 00:58:34 +0100

    Time: 2 ms (67.97%), Space: 55.4 MB (6.12%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,7 +1,5 @@
-// SnowBall Method
-
-class Solution {
-    public void moveZeroes(int[] nums) {
+/* SnowBall Method
+ public void moveZeroes(int[] nums) {
         
         int snowBall = 0;
         for (int i = 0; i < nums.length; i++){
@@ -11,6 +9,21 @@ class Solution {
                 int t = nums[i];
                 nums[i] = 0;
                 nums[i - snowBall] = t;
+            }
+        }
+    }
+*/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;  // tracks the position of next non-zero element
+        
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
         }
     }
