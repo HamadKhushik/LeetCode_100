446b9def8591c81343598c6e223bd9d559175116
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-05-19 20:53:18 +0100

    Time: 6 ms (39.66%), Space: 41.9 MB (77.63%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,26 @@
+// recursive solution
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0){
+            return result;
+        }
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        getCombinations(result, digits, "", 0, mapping);
+        return result;
+    }
+    
+    private void getCombinations(List<String> result, String digits, String current, int index, String[] mapping){
+        if (index == digits.length()){
+            result.add(current);
+            return;
+        }
+        
+        String letters = mapping[digits.charAt(index) - '0'];
+        for (int i = 0; i < letters.length(); i++){
+            getCombinations(result, digits, current + letters.charAt(i), index+1, mapping);
+        }
+    }
+}
