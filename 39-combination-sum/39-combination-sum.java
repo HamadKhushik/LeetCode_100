ed1759306376ed875119bb6638f5a95020b23d8e
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-05-25 00:59:37 +0100

    Time: 3 ms (91.30%), Space: 44.8 MB (46.58%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(candidates);
+        Arrays.sort(candidates);  // useful for large inputs when used in combination with for loop condition
         backtrack(candidates, 0, target, new ArrayList<Integer>(), result);
         return result;
     }
@@ -12,7 +12,7 @@ class Solution {
         } else if (remaining == 0){
             result.add(new ArrayList<Integer>(current));
         } else {
-            for (int i = start; i < candidates.length && candidates[i] <= remaining; i++){
+            for (int i = start; i < candidates.length && candidates[i] <= remaining; i++){  // for loop condition to take advantage of sorted array
                 current.add(candidates[i]);
                 backtrack(candidates, i, remaining - candidates[i], current, result);
                 current.remove(current.size() - 1);
