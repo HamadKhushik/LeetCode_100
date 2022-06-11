37e22a19105a8f823767723c1d92c5c4aa444d99
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-06-11 21:38:06 +0100

    Time: 149 ms (58.29%), Space: 42.2 MB (46.13%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -2,35 +2,39 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         char[] w = word.toCharArray();
         
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(exist(board, i, j, w, 0)){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if(exist(board, w, i , j, 0)){
                     return true;
                 }
             }
         }
         return false;
     }
-        
-        private boolean exist(char[][] board, int x, int y, char[] word, int count){
-            if(count == word.length){
-                return true;
-            }
-            
-            if (x < 0 || y < 0 || x >= board.length || y >= board[0].length){
-                return false;
-            }
-            if (board[x][y] != word[count]){
-                return false;
-            }
-            
-            char temp = board[x][y];
-            board[x][y] = ' ';
-            boolean exist = exist(board, x+1, y, word, count+1) ||
-                            exist(board, x-1, y, word, count+1) ||
-                            exist(board, x, y+1, word, count+1) ||
-                            exist(board, x, y-1, word, count+1);
-            board[x][y] = temp;
-            return exist;
+    
+    private boolean exist(char[][] board, char[] word, int x, int y, int count){
+        if (count == word.length){
+            return true;
         }
+        
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length){
+            return false;
+        }
+        
+        if (board[x][y] != word[count]){
+            return false;
+        }
+        
+        char temp = board[x][y];
+        board[x][y] = ' ';
+        
+        if (exist(board, word, x + 1, y, count + 1) || 
+                 exist(board, word, x - 1, y, count + 1) ||
+                 exist(board, word, x, y + 1, count + 1) ||
+                 exist(board, word, x, y - 1, count  +1)){
+            return true;
+        }
+        board[x][y] = temp;
+        return false;
+    }
 }
