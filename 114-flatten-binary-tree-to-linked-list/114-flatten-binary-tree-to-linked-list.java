6324712ac954f12750b9452b26bea84e6888a533
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-06-28 20:45:54 +0100

    Time: 3 ms (5.72%), Space: 43.4 MB (5.45%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,21 +14,29 @@
  * }
  */
 
-// Reversed preorder Traversal
-class Solution {
-    
-    private TreeNode prev = null;
-    
+// Iterative solution by Nick White
+class Solution {    
     public void flatten(TreeNode root) {
-        if (root == null){
+        if(root == null)
             return;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop();
+            
+            if(current.right != null){
+                stack.push(current.right);
+            }
+            if (current.left != null){
+                stack.push(current.left);
+            }
+            
+            if (!stack.isEmpty()){
+                current.right = stack.peek();
+            }
+            current.left = null;
         }
-        
-        flatten(root.right);
-        flatten(root.left);
-        
-        root.right = prev;
-        root.left = null;
-        prev = root;
     }
 }
