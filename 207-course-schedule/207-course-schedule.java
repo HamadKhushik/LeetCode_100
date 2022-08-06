6f1b84013cb7e1ebcedfcafca01189f54b029cca
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-08-06 16:46:42 +0100

    Time: 195 ms (5.07%), Space: 107.2 MB (5.00%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,46 +1,43 @@
-// Topological sort using Adjacency Matrix
+// Topological sort using Adjacency List implementation
 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        int[][] matrix = new int[numCourses][numCourses];
+        List<Integer>[] list = new List[numCourses];
         int[] inDegree = new int[numCourses];
         
-        // build the graph as Adjacency Matrix
+        for (int i = 0; i < numCourses; i++){
+            list[i] = new ArrayList();
+        }
+        
+        // make the graph, define all the neighbours in list
         for (int i = 0; i < prerequisites.length; i++){
             int ready = prerequisites[i][0];
             int pre = prerequisites[i][1];
-            if (matrix[pre][ready] == 0){
-                inDegree[ready]++;
-                matrix[pre][ready] = 1;
-            }
+            
+            list[pre].add(ready);
+            inDegree[ready]++;
         }
-        
-        // add all the nodes with no incoming edges to queue
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < inDegree.length; i++){
+        for (int i = 0; i < numCourses; i++){
             if (inDegree[i] == 0){
                 queue.offer(i);
             }
         }
-        
         int count = 0;
         
+        // remove prerequisites/edges and update count
         while (!queue.isEmpty()){
             int course = queue.poll();
-            count++;
-            // remove all edges originating from 'course'
+            count++; 
             for (int i = 0; i < numCourses; i++){
-                if (matrix[course][i] == 1){
-                    // if no incoming edges(means all prerequisites have been completed) add it to queue
+                if (list[course].contains(i)){
                     if (--inDegree[i] == 0){
                         queue.offer(i);
                     }
-//               matrix[current][i] = 0;  // this is not necessary, not included in Kahn's algorithm
+ //                   list[course].remove(i);
                 }
             }
         }
-        // if count != numCourses then there is a cycle in the graph
-        return count == numCourses; 
+        return count == numCourses;
     }
 }
