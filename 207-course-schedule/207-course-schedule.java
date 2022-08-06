9a066545d36b692914923f2fbf0fd02e4c7de53c
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-08-06 13:43:13 +0100

    Time: 44 ms (8.03%), Space: 79.1 MB (5.00%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,46 @@
+// Topological sort using Adjacency Matrix
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        int[][] matrix = new int[numCourses][numCourses];
+        int[] inDegree = new int[numCourses];
+        
+        for (int i = 0; i < prerequisites.length; i++){
+            int ready = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            if (matrix[pre][ready] == 0){
+                inDegree[ready]++;
+                matrix[pre][ready] = 1;
+            }
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < inDegree.length; i++){
+            if (inDegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+        
+        int count = 0;
+        
+        while (!queue.isEmpty()){
+            int current = queue.poll();
+            count++;
+            for (int i = 0; i < numCourses; i++){
+                if (matrix[current][i] == 1){
+                    if (--inDegree[i] == 0){
+                        queue.offer(i);
+                    }
+                    matrix[current][i] = 0;
+                }
+            }
+//            for (int j = 0; j < inDegree.length; j++){
+//                if (inDegree[j] == 0){
+//                    queue.offer(j);
+//                }
+//            }
+        }
+        return count == numCourses; 
+    }
+}
