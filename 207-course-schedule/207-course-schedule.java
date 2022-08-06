4c84b0b2d8e3be18723a5b8c6a16b5b14401d80b
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-08-06 13:51:51 +0100

    Time: 56 ms (5.56%), Space: 77.4 MB (5.00%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -6,6 +6,7 @@ class Solution {
         int[][] matrix = new int[numCourses][numCourses];
         int[] inDegree = new int[numCourses];
         
+        // build the graph as Adjacency Matrix
         for (int i = 0; i < prerequisites.length; i++){
             int ready = prerequisites[i][0];
             int pre = prerequisites[i][1];
@@ -15,6 +16,7 @@ class Solution {
             }
         }
         
+        // add all the nodes with no incoming edges to queue
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < inDegree.length; i++){
             if (inDegree[i] == 0){
@@ -25,22 +27,20 @@ class Solution {
         int count = 0;
         
         while (!queue.isEmpty()){
-            int current = queue.poll();
+            int course = queue.poll();
             count++;
+            // remove all edges originating from 'course'
             for (int i = 0; i < numCourses; i++){
-                if (matrix[current][i] == 1){
+                if (matrix[course][i] == 1){
+                    // if no incoming edges(means all prerequisites have been completed) add it to queue
                     if (--inDegree[i] == 0){
                         queue.offer(i);
                     }
-//                    matrix[current][i] = 0;
+//               matrix[current][i] = 0;  // this is not necessary, not included in Kahn's algorithm
                 }
             }
-//            for (int j = 0; j < inDegree.length; j++){
-//                if (inDegree[j] == 0){
-//                    queue.offer(j);
-//                }
-//            }
         }
+        // if count != numCourses then there is a cycle in the graph
         return count == numCourses; 
     }
 }
