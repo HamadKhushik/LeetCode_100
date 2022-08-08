83fea0866fa9e818b0390e9e731900877aa6af9a
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-08-08 01:11:14 +0100

    Time: 27 ms (22.37%), Space: 50 MB (19.22%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,13 +1,44 @@
+// quick selection algorithm using the partition method
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue();
         
-        for (int num : nums){
-            pq.offer(num);
-            if (pq.size() > k){
-                pq.remove();
+        k = nums.length - k;
+        int lo = 0; 
+        int hi = nums.length - 1;
+        
+        while (lo < hi){
+            final int j = partition(nums, lo, hi);
+            
+            if (j < k){
+                lo = j + 1;
+            } else if (j > k){
+                hi = j - 1; 
+            } else {
+                break;
             }
         }
-        return pq.remove();
+        return nums[k];  // j = k as the loop will only break on this condition
+    }
+    
+    private int partition(int[] nums, int lo, int hi){
+        int i = lo;
+        int j = hi + 1;
+        
+        while(true){
+            while((i < hi) && (nums[++i] < nums[lo]));
+            while ((j > lo) && (nums[lo] < nums[--j]));
+            if (i >= j){
+                break;
+            }
+            swap(nums, i , j);
+        }
+        swap(nums, lo, j);
+        return j;
+    }
+    
+    private void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
