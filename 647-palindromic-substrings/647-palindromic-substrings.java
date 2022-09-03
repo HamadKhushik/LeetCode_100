b96afe37b62a5dcbf9f76780ae9f76c499bf75a2
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-09-03 23:33:09 +0100

    Time: 2 ms (98.81%), Space: 42 MB (69.17%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,18 +1,23 @@
+// if total number of elements in an array is odd, mid is i(one element)
+// if total number of elements in an array is even, mid is i &  i +1 (two elements)
+
 class Solution {    
     public int countSubstrings(String s) {
+        
         int count = 0;
         char[] c = s.toCharArray();
+        
         for (int i = 0; i < s.length(); i++){
             
-            count += isPalindrome(c, i, i);  // odd length
-            count += isPalindrome(c, i, i+1);  // even length
+            count += isPalindrome(c, i, i);  // odd
+            count += isPalindrome(c, i, i+1);  // even
         }
-        
         return count;
     }
     
     private int isPalindrome(char[] c, int start, int end){
         int count = 0;
+        // if c[start] == c[end], expand in both directions
         while (start >= 0 && end < c.length && c[start--] == c[end++]){
             count++;
         }
