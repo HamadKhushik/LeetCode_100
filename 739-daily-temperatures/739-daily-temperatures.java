4de490d4b5b9c4d9e84dffe38690c2fc856033bd
HamadKhushik <57306641+HamadKhushik@users.noreply.github.com>
2022-09-04 10:27:30 +0100

    Time: 51 ms (76.69%), Space: 128.3 MB (69.99%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,14 +1,17 @@
+// using Monotnic Stack - decreasing monotonic stack
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         
         int[] result = new int[temperatures.length];
         Deque<Integer> stack = new ArrayDeque<>();
         
-        for (int i = 0; i < temperatures.length; i++){
+        for (int i = 0; i < temperatures.length; i ++){
+            
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
                 int idx = stack.pop();
                 result[idx] = i - idx;
             }
+            
             stack.push(i);
         }
         return result;
